3c90fa76e53393a7e50029dd7ffebe0534b409a5
Andre Durham <61057334+AndroDDD@users.noreply.github.com>
2024-08-21 17:31:52 -0500

    corrected imports for amenitiesPageDataNameAndIcons file

--- app/constants/amenitiesPageDataNameAndIcons.tsx
+++ app/constants/amenitiesPageDataNameAndIcons.tsx
@@ -1,77 +1,76 @@
-import { PageDataAmenitiesList } from "../../types/propertyPageData"
+import { PageDataAmenitiesList } from "@/app/types/propertyPageData";
 
 const amenitiesPageDataNameAndIcons: PageDataAmenitiesList = [
-    {
-        itemName: "airConditioning",
-        itemDisplayText: "Air Conditioning",
-        icon: "airConditioner"
-    },
-    {
-        itemName: "bedding",
-        itemDisplayText: "Bedding",
-        icon: "swivelChair"
-    },
-    {
-        itemName: "cable",
-        itemDisplayText: "Cable",
-        icon: "cableBoxAndRemote"
-    },
-    {
-        itemName: "cookware",
-        itemDisplayText: "Cookware",
-        icon: "kitchenBlender"
-    },
-    {
-        itemName: "tableware",
-        itemDisplayText: "Tableware",
-        icon: "tableAndChairsv2"
-    },
-    {
-        itemName: "heating",
-        itemDisplayText: "Heating",
-        icon: "airConditioner"
-    },
-    {
-        itemName: "iron",
-        itemDisplayText: "Iron",
-        icon: "iron"
-    },
-    {
-        itemName: "kitchen",
-        itemDisplayText: "Kitchen",
-        icon: "swivelChair"
-    },
-    {
-        itemName: "pool",
-        itemDisplayText: "Pool",
-        icon: "pool"
-    },
-    {
-        itemName: "linens",
-        itemDisplayText: "Linens",
-        icon: "bed"
-    },
-    {
-        itemName: "television",
-        itemDisplayText: "Tv",
-        icon: "tvAndRemote"
-    },
-    {
-        itemName: "wifi",
-        itemDisplayText: "Wifi",
-        icon: "wifi"
-    },
-    {
-        itemName: "washerAndDryer",
-        itemDisplayText: "Washer and Dryer",
-        icon: "airConditioner"
-    },
-    {
-        itemName: "computerMonitor",
-        itemDisplayText: "Computer Monitor",
-        icon: "tvAndRemote"
-    },
+  {
+    itemName: "airConditioning",
+    itemDisplayText: "Air Conditioning",
+    icon: "airConditioner",
+  },
+  {
+    itemName: "bedding",
+    itemDisplayText: "Bedding",
+    icon: "swivelChair",
+  },
+  {
+    itemName: "cable",
+    itemDisplayText: "Cable",
+    icon: "cableBoxAndRemote",
+  },
+  {
+    itemName: "cookware",
+    itemDisplayText: "Cookware",
+    icon: "kitchenBlender",
+  },
+  {
+    itemName: "tableware",
+    itemDisplayText: "Tableware",
+    icon: "tableAndChairsv2",
+  },
+  {
+    itemName: "heating",
+    itemDisplayText: "Heating",
+    icon: "airConditioner",
+  },
+  {
+    itemName: "iron",
+    itemDisplayText: "Iron",
+    icon: "iron",
+  },
+  {
+    itemName: "kitchen",
+    itemDisplayText: "Kitchen",
+    icon: "swivelChair",
+  },
+  {
+    itemName: "pool",
+    itemDisplayText: "Pool",
+    icon: "pool",
+  },
+  {
+    itemName: "linens",
+    itemDisplayText: "Linens",
+    icon: "bed",
+  },
+  {
+    itemName: "television",
+    itemDisplayText: "Tv",
+    icon: "tvAndRemote",
+  },
+  {
+    itemName: "wifi",
+    itemDisplayText: "Wifi",
+    icon: "wifi",
+  },
+  {
+    itemName: "washerAndDryer",
+    itemDisplayText: "Washer and Dryer",
+    icon: "airConditioner",
+  },
+  {
+    itemName: "computerMonitor",
+    itemDisplayText: "Computer Monitor",
+    icon: "tvAndRemote",
+  },
+];
 
-]
-
-export { amenitiesPageDataNameAndIcons }
+export { amenitiesPageDataNameAndIcons };
