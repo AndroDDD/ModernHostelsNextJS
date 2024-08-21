bf3ed9beb422ce6625f47d9e1abf1c20a17f307a
Andre Durham <61057334+AndroDDD@users.noreply.github.com>
2024-08-21 17:34:31 -0500

    removed deprecated api data fetch functions for homepage

--- app/generalFunctions/apiDataFetches/homeRoute/fetchImageCollageSectionData.tsx
+++
@@ -1,9 +0,0 @@
-import { CollageSectionData } from "../../../types/collageSectionData";
-
-export const fetchImageCollageSectionData = (): CollageSectionData => {
-  const fetchedCollageSectionData = JSON.parse(
-    window.wpCustomizerThemeSettings.imageCollageSectionData
-  );
-
-  return fetchedCollageSectionData;
-};
