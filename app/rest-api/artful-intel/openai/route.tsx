abc999d7167eed1a2b824b478e1fb2b881ccfcfe
Andre Durham <61057334+AndroDDD@users.noreply.github.com>
2024-08-21 17:47:15 -0500

    removed artful-intel rest endpoint

--- app/rest-api/artful-intel/openai/route.tsx
+++
@@ -1,9 +0,0 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
-import { NextRequest } from "next/server";
-
-export async function POST(request: NextRequest) {}
-const { text } = await generateText({
-  model: openai("gpt-4-turbo"),
-  prompt: "Write a vegetarian lasagna recipe for 4 people.",
-});
