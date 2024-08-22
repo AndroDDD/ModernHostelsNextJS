75a4a719ef11cbb9a48776029a4d2dcc492b9039
Andre Durham <61057334+AndroDDD@users.noreply.github.com>
2024-08-22 18:30:17 -0500

    added fontawesome link to head

--- app/ui/headerAndFooter/header/headerScripts.tsx
+++ app/ui/headerAndFooter/header/headerScripts.tsx
@@ -2,6 +2,10 @@ export default function HeaderScripts() {
   return (
     <>
       <script src="https://www.google.com/recaptcha/enterprise.js?render=6LeJuCIqAAAAAPPnh-6wUCrDOFIk0Btyccwqky4Y"></script>
+      <script
+        src="https://kit.fontawesome.com/392af6cf18.js"
+        crossOrigin="anonymous"
+      ></script>
     </>
   );
 }
