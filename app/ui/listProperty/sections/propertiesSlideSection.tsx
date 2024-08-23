55777a8526dbf368cf71ce4a697406ba73e46088
Andre Durham <61057334+AndroDDD@users.noreply.github.com>
2024-08-22 19:16:11 -0500

    added conditional check for properties length for slide section of list property page

--- app/ui/listProperty/sections/propertiesSlideSection.tsx
+++ app/ui/listProperty/sections/propertiesSlideSection.tsx
@@ -10,83 +10,89 @@ export default ({ properties }: PropertiesSlideSection) => {
 
   return (
     <section id="kst-list-property-page-properties-slide-section">
-      <div className="kst-list-property-page-properties-slide-section-content">
-        <div className="kst-list-property-page-properties-slide-section-content-text">
-          <div className="kst-list-property-page-properties-slide-section-content-title">
-            {properties[currentPropertyIndex].name ?? ""}
-          </div>
+      {properties.length > 0 ? (
+        <>
+          <div className="kst-list-property-page-properties-slide-section-content">
+            <div className="kst-list-property-page-properties-slide-section-content-text">
+              <div className="kst-list-property-page-properties-slide-section-content-title">
+                {properties[currentPropertyIndex].name}
+              </div>
 
-          <div className="kst-list-property-page-properties-slide-section-content-image">
-            <img src={properties[currentPropertyIndex].imgUrl} />
-          </div>
+              <div className="kst-list-property-page-properties-slide-section-content-image">
+                <img src={properties[currentPropertyIndex].imgUrl} />
+              </div>
 
-          <div className="kst-list-property-page-properties-slide-section-content-stats">
-            {properties[currentPropertyIndex].stats.length > 0 ? (
-              properties[currentPropertyIndex].stats.map((stat) => (
-                <div className="kst-list-property-page-properties-slide-section-content-stat">
-                  <div className="kst-list-property-page-properties-slide-section-content-stat-number">
-                    {stat.number}
-                  </div>
+              <div className="kst-list-property-page-properties-slide-section-content-stats">
+                {properties[currentPropertyIndex].stats.length > 0 ? (
+                  properties[currentPropertyIndex].stats.map((stat) => (
+                    <div className="kst-list-property-page-properties-slide-section-content-stat">
+                      <div className="kst-list-property-page-properties-slide-section-content-stat-number">
+                        {stat.number}
+                      </div>
 
-                  <div className="kst-list-property-page-properties-slide-section-content-stat-name">
-                    {stat.name}
-                  </div>
+                      <div className="kst-list-property-page-properties-slide-section-content-stat-name">
+                        {stat.name}
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <></>
+                )}
+              </div>
+
+              <div className="kst-list-property-page-properties-slide-section-content-review">
+                <div className="kst-list-property-page-properties-slide-section-content-review-statement">
+                  {properties[currentPropertyIndex].review.statement}
                 </div>
-              ))
-            ) : (
-              <></>
-            )}
-          </div>
 
-          <div className="kst-list-property-page-properties-slide-section-content-review">
-            <div className="kst-list-property-page-properties-slide-section-content-review-statement">
-              {properties[currentPropertyIndex].review.statement}
+                <div className="kst-list-property-page-properties-slide-section-content-review-from-date">
+                  {`${properties[currentPropertyIndex].review.from} - ${properties[currentPropertyIndex].review.date} Stay`}
+                </div>
+              </div>
             </div>
 
-            <div className="kst-list-property-page-properties-slide-section-content-review-from-date">
-              {`${properties[currentPropertyIndex].review.from} - ${properties[currentPropertyIndex].review.date} Stay`}
+            <div className="kst-list-property-page-properties-slide-section-content-image">
+              <img src={properties[currentPropertyIndex].imgUrl} />
             </div>
           </div>
-        </div>
 
-        <div className="kst-list-property-page-properties-slide-section-content-image">
-          <img src={properties[currentPropertyIndex].imgUrl} />
-        </div>
-      </div>
+          <div className="kst-list-property-page-properties-slide-section-nav-bar">
+            <div
+              className="kst-list-property-page-properties-slide-section-nav-bar-prev"
+              onClick={(event) => {
+                event.preventDefault();
 
-      <div className="kst-list-property-page-properties-slide-section-nav-bar">
-        <div
-          className="kst-list-property-page-properties-slide-section-nav-bar-prev"
-          onClick={(event) => {
-            event.preventDefault();
+                setCurrentPropertyIndex((prevIndex) => {
+                  const updatedIndex =
+                    prevIndex - 1 < 0 ? properties.length - 1 : prevIndex - 1;
 
-            setCurrentPropertyIndex((prevIndex) => {
-              const updatedIndex =
-                prevIndex - 1 < 0 ? properties.length - 1 : prevIndex - 1;
+                  return updatedIndex;
+                });
+              }}
+            >
+              {fetchSVGIcon("simpleLeftArrowWithCircleBorder")}
+            </div>
 
-              return updatedIndex;
-            });
-          }}
-        >
-          {fetchSVGIcon("simpleLeftArrowWithCircleBorder")}
-        </div>
+            <div
+              className="kst-list-property-page-properties-slide-section-nav-bar-next"
+              onClick={(event) => {
+                event.preventDefault();
 
-        <div
-          className="kst-list-property-page-properties-slide-section-nav-bar-next"
-          onClick={(event) => {
-            event.preventDefault();
+                setCurrentPropertyIndex((prevIndex) => {
+                  const updatedIndex =
+                    prevIndex + 1 >= properties.length ? 0 : prevIndex + 1;
 
-            setCurrentPropertyIndex((prevIndex) => {
-              const updatedIndex =
-                prevIndex + 1 >= properties.length ? 0 : prevIndex + 1;
-
-              return updatedIndex;
-            });
-          }}
-        >
-          {fetchSVGIcon("simpleRightArrowWithCircleBorder")}
-        </div>
-      </div>
+                  return updatedIndex;
+                });
+              }}
+            >
+              {fetchSVGIcon("simpleRightArrowWithCircleBorder")}
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </section>
   );
 };
